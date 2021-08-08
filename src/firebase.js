1e33c48a6caa84112bacb36ee336d7c8285a3b46
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2021-08-08 09:39:27 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiMh45vJUO9-_UEepRGp_J1T5yajQVOF4",
-  authDomain: "pinterest-jes.firebaseapp.com",
-  projectId: "pinterest-jes",
-  storageBucket: "pinterest-jes.appspot.com",
-  messagingSenderId: "563761032509",
-  appId: "1:563761032509:web:0fca92a9765cca41066954",
-  measurementId: "G-6THGM1767M",
+  //paste your firebase cdn
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
